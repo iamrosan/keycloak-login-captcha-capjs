fe5bdc5925201f7f7a499eeedd7ed07796ccf2d1
iamrosan <roshanshrestha21.63@gmail.com>
2025-05-12 12:16:10 +0545

    feat: new captcha inheriting userpassword form

--- themes/capjs/login/resources/js/capjs.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const widget = document.querySelector("#cap");
-  if (widget) {
-    widget.addEventListener("solve", function (e) {
-      document.getElementById("cap-token").value = e.detail.token;
-    });
-  }
-});
